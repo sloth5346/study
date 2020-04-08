228cbd97946c2201e80499e3faa294c325a2028e
田中貴弘 <@DESKTOP-2EU1O5E>
2020-04-08 19:22:31 +0900

    DBにデータ投入ができなかった

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+	private HelloService helloService;
+	
 	@GetMapping("/hello")
 	public String getHello() {
 		// hello.htmlに画面遷移
@@ -20,6 +25,25 @@ public class HelloController {
 		model.addAttribute("sample", str);
 		
 		return "helloResponse";
+	}
+	
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2")String str, Model model) {
+		
+		// Stringからint型に変換
+		int id = Integer.parseInt(str);
+		
+		// 1件検索
+		Employee employee = helloService.findOne(id);
+		
+		// 検索結果をModelに登録
+		model.addAttribute("id", employee.getEmployeeId());
+		model.addAttribute("name", employee.getEmployeeName());
+		model.addAttribute("age", employee.getAge());
+		
+		// helloResponseDB.htmlに画面遷移
+		return "helloResponseDB";
+		
 	}
  
 }
---
+++ src/main/java/com/example/controller/HelloRepository.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+@Repository
+public class HelloRepository {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public Map<String, Object> findOne(int id){
+		
+		// SELECT文
+		String query = "SELECT"
+				+ " employee_id,"
+				+ " employee_name,"
+				+ " age"
+				+ " FROM employee"
+				+ " WHERE employee_id=?";
+		
+		// 検索実行
+		Map<String, Object>employee = jdbcTemplate.queryForMap(query, id);
+		
+		return employee;
+		
+		
+	}
+	
+
+}
