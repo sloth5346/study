bbf6f551a77636eedf6d69bc2421768a2a9f134d
田中貴弘 <@DESKTOP-2EU1O5E>
2020-04-13 19:04:50 +0900

    10.3.1　直リンクの禁止 完了

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/webjars/**", "/css/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		.antMatchers("/webjars/**").permitAll()
+		.antMatchers("/css/**").permitAll()
+		.antMatchers("/login").permitAll()
+		.antMatchers("/signup").permitAll()
+		.anyRequest().authenticated();
+		
+		http.csrf().disable();
+	}
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ VALUES(1, '山田太郎', 30);
 
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
 VALUES('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
+
+INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
+VALUES('tamura@xxx.co.jp', 'password', '田村達也', '1986-11-05', 31, false, 'ROLE_GENERAL');
