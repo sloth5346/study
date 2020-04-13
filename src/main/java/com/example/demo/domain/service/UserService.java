b1aadaeb7a2d4e09fed8cf731291cc92f49d87c6
田中貴弘 <@DESKTOP-2EU1O5E>
2020-04-13 13:19:21 +0900

    8.3.7 BeanPropertyRowMapper完了

--- src/main/java/com/example/demo/domain/service/UserService.java
+++ src/main/java/com/example/demo/domain/service/UserService.java
@@ -13,7 +13,7 @@ import com.example.demo.domain.model.repository.UserDao;
 public final class UserService {
 
 	@Autowired
-	@Qualifier("UserDaoJdbcImpl2")
+	@Qualifier("UserDaoJdbcImpl3")
 	UserDao dao;
 
 	public boolean insert(User user) {
