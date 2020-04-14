93b21ee6e1ae9b6c7c39a7067fe5302ada86eb00
田中貴弘 <@DESKTOP-2EU1O5E>
2020-04-14 11:55:20 +0900

    10.3.5 ログアウト処理 まで完了

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,15 +1,48 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	
+	private static final String USER_SQL = "SELECT"
+			+ " user_id,"
+			+ " password,"
+			+ " true,"
+			+ " FROM"
+			+ " m_user"
+			+ " WHERE"
+			+ " user_id=?";
+	
+	private static final String ROLE_SQL = "SELECT"
+			+ " user_id,"
+			+ " role"
+			+ " FROM"
+			+ " m_user"
+			+ " WHERE"
+			+ " user_id=?";
+			
 	@Override
 	public void configure(WebSecurity web) throws Exception{
 		web.ignoring().antMatchers("/webjars/**", "/css/**");
@@ -24,7 +57,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/signup").permitAll()
 		.anyRequest().authenticated();
 		
+		http.formLogin()
+				.loginProcessingUrl("/login")
+				.loginPage("/login")
+				.failureUrl("/login")
+				.usernameParameter("userId")
+				.passwordParameter("password")
+				.defaultSuccessUrl("/home", true);
+				
 		http.csrf().disable();
 	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery(USER_SQL)
+		.authoritiesByUsernameQuery(ROLE_SQL)
+		.passwordEncoder(passwordEncoder());
+
+	}
+
 
 }
--- src/main/java/com/example/demo/domain/model/repository/jdbc/UserDaoJdbcImpl.java
+++ src/main/java/com/example/demo/domain/model/repository/jdbc/UserDaoJdbcImpl.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.model.User;
@@ -18,6 +19,9 @@ public class UserDaoJdbcImpl implements UserDao{
 	
 	@Autowired
 	JdbcTemplate jdbc;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	
 	@Override
 	public int count() throws DataAccessException{
@@ -30,21 +34,25 @@ public class UserDaoJdbcImpl implements UserDao{
 	@Override
 	public int insertOne(User user) throws DataAccessException{
 		
-		int rowNumber = jdbc.update("INSERT INTO m_user (user_id,"
+		String password = passwordEncoder.encode(user.getPassword());
+		
+		String sql = "INSERT INTO m_user (user_id,"
 				+ " password,"
 				+ " user_name,"
 				+ " birthday,"
 				+ " age,"
 				+ " marriage,"
 				+ " role)"
-				+ " VALUES(?,?,?,?,?,?,?)"
-				, user.getUserId()
-				, user.getPassword()
-				, user.getUserName()
-				, user.getBirthday()
-				, user.getAge()
-				, user.isMarriage()
-				, user.getRole());
+				+ " VALUES(?,?,?,?,?,?,?)";
+		
+		int rowNumber = jdbc.update(sql,
+				user.getUserId(),
+				password,
+				user.getUserName(),
+				user.getBirthday(),
+				user.getAge(),
+				user.isMarriage(),
+				user.getRole());
 		
 		return rowNumber;
 	}
@@ -96,20 +104,24 @@ public class UserDaoJdbcImpl implements UserDao{
 	@Override
 	public int updateOne(User user) throws DataAccessException{
 		
-		int rowNumber = jdbc.update("UPDATE M_USER"
+		String password = passwordEncoder.encode(user.getPassword());
+		
+		String sql = "UPDATE M_USER"
 				+ " SET"
 				+ " password = ?,"
 				+ " user_name = ?,"
 				+ " birthday = ?,"
 				+ " age = ?,"
 				+ " marriage = ?"
-				+ " WHERE user_id = ?"
-				, user.getPassword()
-				, user.getUserName()
-				, user.getBirthday()
-				, user.getAge()
-				, user.isMarriage()
-				, user.getUserId());
+				+ " WHERE user_id = ?";
+		
+		int rowNumber = jdbc.update(sql,
+				password,
+				user.getUserName(),
+				user.getBirthday(),
+				user.getAge(),
+				user.isMarriage(),
+				user.getUserId());
 		
 		//if(rowNumber > 0) {
 		//	throw new DataAccessException("トランザクションテスト") {};
