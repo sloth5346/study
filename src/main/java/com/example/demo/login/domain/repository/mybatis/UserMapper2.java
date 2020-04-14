9547322d12e5d697ec36d5291f9942feddbce41a
田中貴弘 <@DESKTOP-2EU1O5E>
2020-04-14 17:30:02 +0900

    13.4 CRUD操作(設定ファイルによる実装)

--- src/main/java/com/example/demo/login/domain/repository/mybatis/UserMapper2.java
+++ src/main/java/com/example/demo/login/domain/repository/mybatis/UserMapper2.java
@@ -2,22 +2,20 @@ package com.example.demo.login.domain.repository.mybatis;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.login.domain.model.User;
 
+@Mapper
 public interface UserMapper2 {
 
 	public boolean insert(User user);
-	
+
 	public User selectOne(String userId);
-	
+
 	public List<User> selectMany();
-	
+
 	public boolean updateOne(User user);
-	
+
 	public boolean deleteOne(String userId);
 }
